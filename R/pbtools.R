c8ec399faa764e3a80e8f8e163112e9b7c6de2e9
Petr Bouchal <pbouchal@gmail.com>
2014-05-11 11:39:40 +0100

    Including other files in commit

---
+++ R/pbtools.R
@@ -0,0 +1,13 @@
+#' pbtools.
+#'
+#' @name pbtools
+#' @docType package
+NULL
+
+#' @docType data
+#' @keywords datasets
+#' @name themecolours
+#' @title Colour set for theme
+#' @usage data(themecolours)
+#' @format Matrix with 4 columns and 6 rows - 4 tints (25% steps) of 6 colours
+NULL
