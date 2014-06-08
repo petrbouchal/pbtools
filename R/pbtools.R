d77c571e337e0ca6c2c1521ed95e2df6ed002b74
Petr Bouchal <pbouchal@gmail.com>
2014-06-08 22:10:01 +0100

    Initial code for object saving wrapper function

--- R/pbtools.R
+++ R/pbtools.R
@@ -7,15 +7,31 @@ NULL
 #' @docType data
 #' @keywords datasets
 #' @name themecolours
-#' @title Colour set for theme
+#' @title Colour set for theme - 4 tints of 6 colours
 #' @usage data(themecolours)
-#' @format Matrix with 4 columns and 6 rows - 4 tints (25\% steps) of 7 colours
+#' @format Matrix with 4 columns and 6 rows - 4 tints (25\% steps) of 6 colours
 NULL
 
 #' @docType data
 #' @keywords datasets
 #' @name ifgbasecolours
-#' @title Colour set for theme - IfG corporate colours
+#' @title Colour set for theme - IfG corporate colours - 7 base colours only
+#' @usage data(themecolours)
+#' @format Matrix with 1 column and 7 rows - 7 base colours only
+NULL
+
+#' @docType data
+#' @keywords datasets
+#' @name ifgcolours
+#' @title Colour set for theme - IfG corporate colours - 4 tints of 7 colours
 #' @usage data(themecolours)
 #' @format Matrix with 4 columns and 7 rows - 4 tints (25\% steps) of 7 colours
+NULL
+
+#' @docType data
+#' @keywords datasets
+#' @name themebasecolours
+#' @title Colour set for theme - default ugly colours (6 base colours only)
+#' @usage data(themecolours)
+#' @format Matrix with 4 columns and 6 rows - 4 tints (25\% steps) of 6 colours
 NULL
