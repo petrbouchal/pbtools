78d89b3110c5284a9a3524504057561398b57696
Petr Bouchal <pbouchal@gmail.com>
2014-06-08 22:10:01 +0100

    Initial code for object saving wrapper function

---
+++ R/savefilewithdate.R
@@ -0,0 +1,19 @@
+#' Save file with timestamp, with option for time-stamped
+#'
+#' Save object into file
+#' @param object Ggplot object.
+#' @param plotname String. Defaults 'Plot'.
+#' @param ffamily Font family. Defaults to 'Helvetica'
+#' @param plotformat String
+#' @param plotw Width of plot in centimeters. Defaults to 14
+#' @param ploth Height of plot in centimeters. Defaults to 11.5
+#' @param plotdir Directory for saving - plot, data and image are saved into subdirectories
+#' @keywords data, file, save, date
+#' @export
+#' @return NULL
+#' @examples
+#' savefilewithdate()
+
+savefilewithdate <- function() {
+  return(TRUE)
+}
