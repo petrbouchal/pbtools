6a527261377dfa2865880333aaf69fe71c8c2ffc
Petr Bouchal <pbouchal@gmail.com>
2014-06-08 22:07:44 +0100

    reorganise theme colours

--- R/MarkSumGroups.R
+++
@@ -1,7 +0,0 @@
-MarkSumGroups <- function(data) {
-  data$sgrp <- TRUE
-  data$sgrp[data$grp=='UO - Ministerstva' | data$grp=='UO - Ostatní' |
-            data$grp=='PO' | data$grp=='OOSS' | data$grp=='OSS-SS' |
-            data$grp=='SOBCPO'] <- FALSE
-  return(data)
-}
